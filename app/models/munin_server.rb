a860aa51d26cc2d37b00093d3be35546dfc9ff43
Renaud Chaput <renchap@cocoa-x.com>
2009-02-26 14:17:27 +0100

    Added MuninServer#fetch_servers_list
    This method fetches the main Munin page and creates all missing servers.
    It uses Hpricot to parse the data. HTTP authentication not working.

--- app/models/munin_server.rb
+++ app/models/munin_server.rb
@@ -1,3 +1,20 @@
+require 'open-uri'
+
 class MuninServer < ActiveRecord::Base
   has_many :servers
+
+  def fetch_servers_list
+    # Fetch the munin homepage
+    doc = Hpricot(open(self.url))
+    
+    # Search all servers
+    (doc/"span.host"/"a").each do |server|
+      server_name = server.inner_html
+      unless self.servers.find(:first, :conditions => { :hostname => server_name })
+       puts "Adding "+server_name
+        self.servers.create(:hostname => server_name)
+      end
+    end
+    return true
+  end
 end
