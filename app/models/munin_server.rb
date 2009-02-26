2fd08b4f1ae4a2c68f6f43a906f24e1232e5307c
Renaud Chaput <renchap@cocoa-x.com>
2009-02-26 14:33:28 +0100

    Handle HTTP authentication

--- app/models/munin_server.rb
+++ app/models/munin_server.rb
@@ -4,14 +4,20 @@ class MuninServer < ActiveRecord::Base
   has_many :servers
 
   def fetch_servers_list
+    
     # Fetch the munin homepage
-    doc = Hpricot(open(self.url))
+    if self.http_user
+      content = open(self.url, :http_basic_authentication => [self.http_user, self.http_password])
+    else
+      content = open(self.url)
+    end
+    
+    doc = Hpricot(content)
     
     # Search all servers
     (doc/"span.host"/"a").each do |server|
       server_name = server.inner_html
       unless self.servers.find(:first, :conditions => { :hostname => server_name })
-       puts "Adding "+server_name
         self.servers.create(:hostname => server_name)
       end
     end
