80578dc9c020eca4cb8b5f43c56e274ec0d34a3a
Renaud Chaput <renchap@cocoa-x.com>
2009-03-09 14:16:15 +0100

    Added a page with server details

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,9 +1,20 @@
 class ServersController < ApplicationController
+  before_filter :find_by_param, :except => [:index]
   def index
     @servers = Server.all(:order => 'hostname ASC')
   end
 
   def show
+    
   end
 
+  protected
+    def find_by_param
+      begin
+        @server = Server.find(params['id'])
+      rescue(ActiveRecord::RecordNotFound)
+        redirect_to servers_path
+        false
+      end
+    end
 end
