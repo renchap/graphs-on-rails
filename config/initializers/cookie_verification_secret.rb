61c30dbf54a397444f5e0559db710cd1599e55f7
Renaud Chaput <renchap@cocoa-x.com>
2010-02-27 20:11:53 +0100

    Bare Rails 3 install

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'b638017c4c69adf216b60e8d5a1ff19adeb1b2d00dfe9d69f70643672b7ea338094ead71fb95cf248689a9470728a553f474c6eb0208a8854fc47b6ffabfbd7d'
