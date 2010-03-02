9aab87b9f9e528763b73d9d2977d90d8f605f8e5
Renaud Chaput <renchap@cocoa-x.com>
2010-03-02 17:40:16 +0100

    List all hosts

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,5 +1,9 @@
 class HostsController < ApplicationController
   def index
+    @hosts = Host.find(:all, :order => 'name ASC')
   end
-
+  
+  def show
+    @host = Host.from_param(params[:id])
+  end
 end
--- app/models/host.rb
+++ app/models/host.rb
@@ -1,3 +1,11 @@
 class Host < ActiveRecord::Base
   has_many :metrics
+  
+#  def to_param
+#    self.name
+#  end
+  
+  def self.from_param(param)
+    find(param)
+  end
 end
