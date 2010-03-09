67366d6cdb487d48a0236535b99d58454ff25b54
Renaud Chaput <renchap@cocoa-x.com>
2010-03-09 13:29:42 +0100

    Avoid a call to RRD-FFI to get RRA names.

--- lib/data_providers/collectd/collectd.rb
+++ lib/data_providers/collectd/collectd.rb
@@ -51,7 +51,11 @@ module DataProvider
         rrd = RRD::Base.new(options[:path])
         results = rrd.fetch(:average, :start => start_date, :end => end_date)
         r = Array.new
-        index = RRDTool::rrd_index_for(rrd, options[:rra]) + 1
+        
+        index = results.first.index(options[:rra])
+        # Delete first line of results as it contains rra names
+        results.delete_at(0)
+        
         results.each do |line|
           r << [line[0], line[index]] if line[index] and line[index].class == Float and !line[index].nan?
         end
--- lib/data_providers/rrdtool/rrdtool.rb
+++ lib/data_providers/rrdtool/rrdtool.rb
@@ -6,16 +6,5 @@ module DataProvider
     class Provider < Base      
       DataProvider.register(self)
     end
-    
-    def self.rrd_index_for(rrd, rra)
-      infos = rrd.info
-      # Find all rra
-      infos.each do |name,val|
-        if name =~ /^ds\[([0-9a-zA-Z]+)\]\.index$/
-          return val if $1 == rra
-        end
-      end
-      throw Exception.new('Cant find this RRA in the RRD file')
-    end
   end
 end
