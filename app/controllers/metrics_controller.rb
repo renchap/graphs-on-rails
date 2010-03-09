1b251280e3de9445aaadb0afb214a97061817153
Renaud Chaput <renchap@cocoa-x.com>
2010-03-09 23:05:59 +0100

    Flot options are loaded from the JSON, added support for metric unit

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -7,8 +7,16 @@ class MetricsController < ApplicationController
       format.html
       format.json do
         result = Hash.new
-        result[:label] = @metric.name
-        result[:data] = @metric.get_data(Time.now-1.hour, Time.now).map do |x|
+        result[:options] = Hash.new
+        result[:options][:lines] = { :show => :true, :fill => :true}
+        result[:options][:xaxis] = { :mode => :time }
+        result[:options][:yaxis] = Hash.new
+        result[:options][:yaxis][:min] = @metric.options[:minimum] if @metric.options[:minimum]
+        result[:options][:yaxis][:tickFormatter] = @metric.options[:unit] if @metric.options[:unit]
+        result[:series] = Array.new
+        result[:series][0] = Hash.new
+        result[:series][0][:label] = @metric.name
+        result[:series][0][:data] = @metric.get_data(Time.now-1.hour, Time.now).map do |x|
           [x[0]*1000,x[1]]
         end
         
