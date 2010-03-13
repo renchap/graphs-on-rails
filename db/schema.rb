48cbac359024f1030bfb588c6c42492d6152f52e
Renaud Chaput <renchap@cocoa-x.com>
2010-03-13 10:19:21 +0100

    Added a migration to convert metric paths to relative.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100313080009) do
+ActiveRecord::Schema.define(:version => 20100313085226) do
 
   create_table "hosts", :force => true do |t|
     t.string   "name"
