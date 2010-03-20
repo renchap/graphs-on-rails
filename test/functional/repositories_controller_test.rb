eeaf7d67655669f23aed04abdd4de72d1767785a
Renaud Chaput <renchap@cocoa-x.com>
2010-03-20 20:41:35 +0100

    Added tests for Repositories controller.

--- test/functional/repositories_controller_test.rb
+++ test/functional/repositories_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class RepositoriesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Repository index lists all repositories" do
+    get :index
+    assert_response :success
+    assert_equal 1, assigns['repositories'].size
+    assert_select 'ul.repositories' do
+      assert_select 'li.repository', 1
+    end
+  end
+
+  test "repository detail page" do
+    repos = repositories(:collectd)
+    get :show, :id => repos
+    assert_response :success
+    assert_equal repos, assigns['repository']
   end
 end
