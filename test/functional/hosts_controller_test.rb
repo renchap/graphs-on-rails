47953921451f12e459ff3d7e790003fb2213c6dd
Renaud Chaput <renchap@cocoa-x.com>
2010-03-20 19:45:23 +0100

    Added functional tests for Host controller.

--- test/functional/hosts_controller_test.rb
+++ test/functional/hosts_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class HostsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "host index is correct" do
+    get :index
+    assert_equal 2, assigns['hosts'].size
+    assert_select 'ul.hosts' do
+      assert_select 'li.host', 2
+    end
+  end
+
+  test "show host" do
+    host = hosts(:host1)
+    get :show, :id => host
+    assert_equal host, assigns['host']
+    assert_select 'ul.metrics' do
+      assert_select 'li.metric', 2
+    end
   end
 end
