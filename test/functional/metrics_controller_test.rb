6fce5baf57fad6c399de9aaca91f182f9bdb30ab
Renaud Chaput <renchap@cocoa-x.com>
2010-03-24 12:14:05 +0100

    Add a test for the Metrics controller.
    Still need to check the JSON data, but we need to mock calls to RRD.

--- test/functional/metrics_controller_test.rb
+++ test/functional/metrics_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class MetricsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "metric detail page" do
+    metric = metrics(:memory)
+    get :show, :id => metric.id
+    assert_response :success
+    assert_equal metric, assigns['metric']
   end
+
+# TODO: add a mock for RRD calls to test this
+#  test "metric json" do
+#    metric = metrics(:memory)
+#    get :show, :id => metric, :format => :json
+#    assert_response :success
+#    assert_equal metric, assigns['metric']
+#  end
 end
